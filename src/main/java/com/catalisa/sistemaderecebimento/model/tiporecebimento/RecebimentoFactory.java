c93548cc39a91bb35fc5490b03e358d41363bd29
anacmaciel <casmaciel96@gmail.com>
2022-08-19 18:43:02 -0300

    subindo alteração na linha 7 da classe factory, insercao de um )

--- src/main/java/com/catalisa/sistemaderecebimento/model/tiporecebimento/RecebimentoFactory.java
+++ src/main/java/com/catalisa/sistemaderecebimento/model/tiporecebimento/RecebimentoFactory.java
@@ -4,7 +4,7 @@ public class RecebimentoFactory {
     public CalculoRecebimento calculoRecebimento(String tipoRecebimento) {
         if (tipoRecebimento.equalsIgnoreCase("cartao_de_credito")) {
             return new CartaoDeCredito();
-        } else if (tipoRecebimento.equalsIgnoreCase("cartao_de_debito") {
+        } else if (tipoRecebimento.equalsIgnoreCase("cartao_de_debito")) {
             return new CartaoDeDebito();
         } else if (tipoRecebimento.equalsIgnoreCase("vale_refeicao")) {
             return new ValeRefeicao();
